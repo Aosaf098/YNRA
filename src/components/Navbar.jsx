8d8a270374e4d3f8eae4ef6636c6953f2a2f5fed
Aosaf098 <aosafibad10@gmail.com>
2024-10-21 22:01:06 +0600

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="flex justify-around items-center p-4">
+            <div className="flex items-center gap-2">
+                <img className="w-14" src="https://img.icons8.com/?size=48&id=21735&format=png" alt="Liverpool Logo" />
+                <h3 className="text-4xl font-prociono font-bold">YNRA</h3>
+            </div>
+            <div>
+                <img className="w-14" src="https://img.icons8.com/?size=48&id=84690&format=png" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
